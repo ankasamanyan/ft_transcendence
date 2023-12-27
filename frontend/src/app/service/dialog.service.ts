3198bf9d3b4977108f14eca76a37ce0938036f2e
Maria Okatova <mariia.okatova@volkswagen.de>
2023-12-27 16:54:12 +0100

    add getDialog method to dialog service

--- frontend/src/app/service/dialog.service.ts
+++ frontend/src/app/service/dialog.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {SelectedDialog} from "../domain/SelectedDialog";
+import {SelectedDialogResponse} from "./dto/selected-dialog.dto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
   constructor(private httpClient: HttpClient) { }
+
+  getDialog(senderId: string, receiverId: string): Observable<SelectedDialog> {
+    return this.httpClient.get<SelectedDialogResponse>("/selected-dialog/" + senderId + receiverId).pipe(
+      map((dialog: SelectedDialogResponse) => {
+        return SelectedDialogResponse.toDomain(dialog);
+      }));
+  }
 }
