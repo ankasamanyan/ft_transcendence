88ecba5c5bad926c39a594b40b07e4c6de4052a9
Maria Okatova <mariia.okatova@volkswagen.de>
2023-12-30 14:11:33 +0100

    enable search for dialogs

--- frontend/src/app/chat/dialogs/dialogs.component.ts
+++ frontend/src/app/chat/dialogs/dialogs.component.ts
@@ -8,6 +8,12 @@ import {Dialog} from "../../domain/dialog";
 })
 export class DialogsComponent {
   dialogsScrollable: boolean = false;
+  dialogs: Dialog[] = [];
+  displayedDialogs: Dialog[] = [];
+
+  constructor() {
+    this.initializeDialogs();
+  }
   makeDialogsScrollable() {
     this.dialogsScrollable = true;
   }
@@ -16,36 +22,47 @@ export class DialogsComponent {
     this.dialogsScrollable = false;
   }
 
-  dialogs: Dialog[] = [
-    new Dialog(
-      "Cedric",
-      "assets/placeholderComrade2.jpeg",
-      "I need cookies👉👈",
-      "9.25"),
-    new Dialog(
-    "Tetiana",
-    "assets/placeholderComrade.jpeg",
-    "Oh well, you know, so distinguished",
-    "22:25"),
-    new Dialog(
-      "Krisi",
-      "assets/placeholderComrade3.jpeg",
-      "So, I am curious: have you ever...",
-      "19:04"),
-    new Dialog(
-      "Santiago",
-      "assets/placeholderComrade4.jpeg",
-      "Trying to reboot yet again, honestly...",
-      "18:45"),
-    new Dialog(
-      "Fedia",
-      "assets/placeholderComrade5.jpeg",
-      "This laptop is heavy enough to kill people",
-      "16:56"),
-    new Dialog(
-      "Wolf",
-      "assets/placeholderComrade6.jpeg",
-      "Just check my github, it's all there",
-      "14:02")
-  ];
+  find(dialogToSearchFor: string) {
+    if (dialogToSearchFor == "") {
+      this.displayedDialogs = this.dialogs;
+    } else {
+      this.displayedDialogs = this.dialogs.filter((dialog) => dialog.name?.startsWith(dialogToSearchFor));
+    }
+  }
+
+  initializeDialogs() {
+    this.dialogs = [
+      new Dialog(
+        "Cedric",
+        "assets/placeholderComrade2.jpeg",
+        "I need cookies👉👈",
+        "9.25"),
+      new Dialog(
+        "Tetiana",
+        "assets/placeholderComrade.jpeg",
+        "Oh well, you know, so distinguished",
+        "22:25"),
+      new Dialog(
+        "Krisi",
+        "assets/placeholderComrade3.jpeg",
+        "So, I am curious: have you ever...",
+        "19:04"),
+      new Dialog(
+        "Santiago",
+        "assets/placeholderComrade4.jpeg",
+        "Trying to reboot yet again, honestly...",
+        "18:45"),
+      new Dialog(
+        "Fedia",
+        "assets/placeholderComrade5.jpeg",
+        "This laptop is heavy enough to kill people",
+        "16:56"),
+      new Dialog(
+        "Wolf",
+        "assets/placeholderComrade6.jpeg",
+        "Just check my github, it's all there",
+        "14:02")
+    ];
+    this.displayedDialogs = this.dialogs;
+  }
 }
