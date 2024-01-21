f83e8baca78f7a5b9bdcfda8919a819f9483653b
Heather Rothery <hrothery@students.42wolfsburg.de>
2024-01-21 12:23:45 +0100

    simple backend MessageHistory

--- backend/src/message-history/message-history.service.ts
+++ backend/src/message-history/message-history.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { MESSAGE_HISTORY } from 'src/mocks/messages.mock';
 import { MessageDto } from './dto/message.dto';
+//import { PrismaClient, Message } from '@prisma/client';
 
 @Injectable()
 export class MessageHistoryService {
+	//constructor(private readonly prisma: PrismaClient) {}
 	messageHistory: MessageDto[] = MESSAGE_HISTORY;
 
 	getMessageHistory(): Promise<any> {
