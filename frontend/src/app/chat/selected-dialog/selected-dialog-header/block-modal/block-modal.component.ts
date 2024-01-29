003c8fe0a1dbb014ebcd92069000ba9a13f023f3
Maria Okatova <mariia.okatova@volkswagen.de>
2024-01-29 20:43:59 +0100

    make block-modal personalized

--- frontend/src/app/chat/selected-dialog/selected-dialog-header/block-modal/block-modal.component.ts
+++ frontend/src/app/chat/selected-dialog/selected-dialog-header/block-modal/block-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-block-modal',
@@ -8,4 +8,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class BlockModalComponent {
   @Output()
   modalClose = new EventEmitter<void>();
+
+  @Input()
+  selectedPerson: string | undefined;
 }
