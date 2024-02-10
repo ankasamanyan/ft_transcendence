3536561441ec74c1a5eecfec323a0fd86e498dd2
Maria Okatova <mariia.okatova@volkswagen.de>
2024-02-10 23:27:02 +0100

    add name to the modal

--- frontend/src/app/chat/selected-dialog/selected-dialog-header/edit-channel-modal/edit-channel-modal.component.ts
+++ frontend/src/app/chat/selected-dialog/selected-dialog-header/edit-channel-modal/edit-channel-modal.component.ts
@@ -1,12 +1,21 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {User} from "../../../../domain/user";
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {Channel} from "../../../../domain/channel";
 
 @Component({
   selector: 'app-edit-channel-modal',
   templateUrl: './edit-channel-modal.component.html',
   styleUrls: ['./edit-channel-modal.component.css']
 })
-export class EditChannelModalComponent {
+export class EditChannelModalComponent implements AfterViewInit{
   @Output()
   modalClose = new EventEmitter<void>();
+
+  @Input()
+  channel: Channel | undefined;
+
+  @ViewChild('name') channelName!: ElementRef;
+
+  ngAfterViewInit() {
+    this.channelName.nativeElement.focus();
+  }
 }
