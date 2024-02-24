82637f4a8019d68639cf7a2b122e82381d37b9df
Tetiana Fedorenko <tetiana.fedorenko@oparco.de>
2024-02-24 14:56:58 +0100

    add logout

--- frontend/src/app/navigation-bar/exit-modal/exit-modal.component.ts
+++ frontend/src/app/navigation-bar/exit-modal/exit-modal.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {Router} from "@angular/router";
+import {Cookie} from "ng2-cookies/ng2-cookies";
 
 @Component({
   selector: 'app-exit-modal',
@@ -6,6 +8,15 @@ import {Component, EventEmitter, Output} from '@angular/core';
   styleUrls: ['./exit-modal.component.css']
 })
 export class ExitModalComponent {
+
+  constructor(private router: Router){}
+
   @Output()
   modalClose = new EventEmitter<void>();
+
+  logout(){
+    this.router.navigate(['/authorization']).then()
+    localStorage.removeItem('tr_access_token')
+    Cookie.delete('id')
+  }
 }
