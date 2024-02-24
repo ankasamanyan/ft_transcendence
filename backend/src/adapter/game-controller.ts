7cb0fac83a1a4599096f65b45015b9ecc1bcb8b9
ankasamanyan <anikakasamanyan@gmail.com>
2024-02-24 13:34:34 +0100

    fix: paddleUpdate going into ball fixed

--- backend/src/adapter/game-controller.ts
+++ backend/src/adapter/game-controller.ts
@@ -1,43 +1,51 @@
 import {Body, Controller, Get, Param, Post, Put, Delete} from '@nestjs/common';
 import { UsersRequest} from "./dto/users-request";
 import { GameService } from 'src/service/game-service';
+import { PrismaGameRepository } from './repository/prisma-game-repository';
 
 @Controller('/game')
 export class GameController {
-	constructor(private gameService: GameService) {}
+    constructor(private gameService: GameService,
+        private prismaGameRepository: PrismaGameRepository
+        ) {}
 
-	@Post('/invite')
-	invite(@Body() request: UsersRequest) {
-		return this.gameService.invite(UsersRequest.toDomain(request));
-	}
+    @Post('/invite')
+    invite(@Body() request: UsersRequest) {
+        return this.gameService.invite(UsersRequest.toDomain(request));
+    }
 
-	@Delete('/invite/:invitationId')
-	deleteOrDecline(@Param('invitationId') invitationId: number) {
-		return this.gameService.deleteOrDecline(invitationId);
-	}
+    @Delete('/invite/:invitationId')
+    deleteOrDecline(@Param('invitationId') invitationId: number) {
+        return this.gameService.deleteOrDecline(invitationId);
+    }
 
-	@Get('/invitations/:recipientId')
-	getInvitations(@Param('recipientId') recipientId: number) {
-		return this.gameService.getInvitations(recipientId);
-	}
+    @Get('/invitations/:recipientId')
+    getInvitations(@Param('recipientId') recipientId: number) {
+        return this.gameService.getInvitations(recipientId);
+    }
 
-	@Get('/future-matches/:userId')
-	getFutureMatches(@Param('userId') userId: number) {
-		return this.gameService.getFutureMatches(userId);
-	}
+    @Get('/future-matches/:userId')
+    getFutureMatches(@Param('userId') userId: number) {
+        return this.gameService.getFutureMatches(userId);
+    }
 
-	@Put('/invite/:invitationId')
-	accept(@Param('invitationId') invitationId: number) {
-		return this.gameService.accept(invitationId);
-	}
+    @Put('/invite/:invitationId')
+    accept(@Param('invitationId') invitationId: number) {
+        return this.gameService.accept(invitationId);
+    }
 
-	@Get('/future-matches')
-	getAllFutureMatches() {
-		return this.gameService.getAllFutureMatches();
-	}
+    @Get('/future-matches')
+    getAllFutureMatches() {
+        return this.gameService.getAllFutureMatches();
+    }
 
-	@Get('/next-match')
-	getNextMatch() {
-		return this.gameService.getNextMatch();
-	}
+    @Get('/next-match')
+    getNextMatch() {
+        return this.gameService.getNextMatch();
+    }
+
+    @Get('/history/:userId')
+    getMatchHistory(@Param('userId') userId: number) {
+        return this.prismaGameRepository.matchHistory(userId);
+    }
 }
