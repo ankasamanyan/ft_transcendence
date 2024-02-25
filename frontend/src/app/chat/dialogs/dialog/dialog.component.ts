03f73c25e404788d5f52c5b95e5802eaf8107bb8
Maria Okatova <mariia.okatova@volkswagen.de>
2024-02-25 18:12:41 +0100

    hide last message of the found password protected channel

--- frontend/src/app/chat/dialogs/dialog/dialog.component.ts
+++ frontend/src/app/chat/dialogs/dialog/dialog.component.ts
@@ -1,19 +1,40 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Channel} from "../../../domain/channel";
+import {User} from "../../../domain/user";
+import {ChannelService} from "../../../service/channel.service";
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
   @Input()
   channel: Channel | undefined;
+
+  @Input()
+  authenticatedUser: User | undefined;
+
+  participants: User[] | undefined;
+
+  constructor(private channelService: ChannelService) {
+  }
+
+  ngOnInit() {
+    this.channelService.getChannelParticipants(this.channel?.id!).subscribe((value) => {
+      this.participants = value.users;
+    });
+  }
 
   getTime() {
     if (this.channel!.lastMessageCreatedAt) {
       return new Date(this.channel!.lastMessageCreatedAt!).getHours()+ "." + new Date(this.channel!.lastMessageCreatedAt!).getMinutes().toString().padStart(2, "0");
     }
     return "";
+  }
+
+  shouldShowMessage() {
+    return this.channel?.type !== "password-protected" && this.participants?.some((user) =>
+      user.id === this.authenticatedUser?.id)
   }
 }
